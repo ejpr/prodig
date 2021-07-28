08cda360ee1e30488c65e1294620002e012fb25f
Eduardo Parra <eduardo@poincenot.com>
2021-07-28 19:41:19 -0300

    add handler exception for UserNotFound and general Exception

--- src/main/java/com/challenge/prodig/controller/CustomExceptionHandler.java
+++ src/main/java/com/challenge/prodig/controller/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.challenge.prodig.controller;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.challenge.prodig.dto.ErrorResponse;
+import com.challenge.prodig.exception.PersonNotFoundException;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -28,6 +30,30 @@ public class CustomExceptionHandler {
 
         ErrorResponse errorResponse = ErrorResponse.makeErrorResponse(HttpStatus.BAD_REQUEST, errorList);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(PersonNotFoundException.class)
+    protected static ResponseEntity<Object> handlePersonNotFoundException(PersonNotFoundException ex) {
+
+        // @formatter:off
+        List<String> errorList = Collections.singletonList(ex.getMessage());
+        // @formatter:on
+
+        ErrorResponse errorResponse = ErrorResponse.makeErrorResponse(HttpStatus.CONFLICT, errorList);
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected static ResponseEntity<Object> handleGeneralException(Exception ex) {
+
+        // @formatter:off
+        List<String> errorList = Collections.singletonList("Generic error");
+        // @formatter:on
+
+        ErrorResponse errorResponse = ErrorResponse.makeErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, errorList);
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 

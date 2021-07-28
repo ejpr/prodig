565dc2b8a25676be98c07e84026a3caa0fe3f8cd
Eduardo Parra <eduardo@poincenot.com>
2021-07-27 23:17:31 -0300

    spring boot + git ignore.

---
+++ src/main/java/com/challenge/prodig/ProdigApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.prodig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdigApplication.class, args);
+	}
+
+}
